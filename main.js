4640069c3be4c0857d9cf33cb8672d6498e11b4c
Yaser.ta <yasertarrab10@gmail.com>
2024-11-27 20:55:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let uzunluk = 10;
+let genislik = 5;
+let alan = uzunluk * genislik;
+console.log("Dikdörtgen alani:" +alan);
+
+
+let num1 = 15;
+if ( num1> 10 && num1 < 20) {
+    console.log("sayi 10 buyuk ve 20 kucuk");
+} else {
+    console.log("sayin 10 kucuk ve 20 kucuk");
+}
+
+
+let num2 = 3;
+if (num2< 5 || num2> 15) 
+    {
+    console.log("sayi 5'ten kucuk veya 15'ten kucuk.");
+} else {
+    console.log("sayi 5'ten buyuk veya 15'en buyuk");
+}
+
+
+
+let num_1 = 9; 
+if (num_1 % 3 === 0) {
+    console.log("sayi 3'e bolunur");
+} else {
+    console.log("sayi 3'e bolunmez");
+}
+
+// إيجاد باقي القسمة عند تقسيم 100 على 7
+let sayi1= 100;
+let sayi2= 7;
+let bolumden_kalan = sayi1 % sayi2;
+console.log("100 bolu 7'den kalan: " + bolumden_kalan);
+
